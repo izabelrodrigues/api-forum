a4e67d14d021119bba88dc1cbc7b3f9c67295cfc
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2022-07-14 20:46:02 -0300

    Configurando rota de autenticação com permissão para todos

--- src/main/java/br/com/izabelrodrigues/apiforum/config/SecurityConfiguration.java
+++ src/main/java/br/com/izabelrodrigues/apiforum/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,9 +31,10 @@ public class SecurityConfiguration {
 		http.authorizeRequests() //
 				.antMatchers(HttpMethod.GET, "/topicos").permitAll() //
 				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll() //
-				.anyRequest().authenticated().and().formLogin();
-
-		http.headers().frameOptions().sameOrigin();
+				.antMatchers(HttpMethod.POST, "/auth").permitAll() //
+				.anyRequest().authenticated() //
+				.and().csrf().disable() //
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		return http.build();
 	}
