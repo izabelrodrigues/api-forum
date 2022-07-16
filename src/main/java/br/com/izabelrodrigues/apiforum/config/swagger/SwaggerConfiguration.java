d37255c1e329d7dac2c1c64553ea9b3d9cd06881
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2022-07-16 15:28:52 -0300

    Adicionando campo de autorização no swagger

--- src/main/java/br/com/izabelrodrigues/apiforum/config/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/izabelrodrigues/apiforum/config/swagger/SwaggerConfiguration.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 /**
  * @author Izabel Rodrigues
@@ -33,7 +36,15 @@ public class SwaggerConfiguration {
 		Info info = new Info();
 		info.setTitle(title);
 
-		return new OpenAPI().info(info.description(description).version(version));
+		SecurityRequirement securityItem = new SecurityRequirement();
+		securityItem.addList("Authorization");
+
+		Components components = new Components();
+		components.addSecuritySchemes("Authorization", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"));
+
+		return new OpenAPI().components(components)
+				.addSecurityItem(securityItem)
+				.info(info.description(description).version(version));
 	}
 
 }
